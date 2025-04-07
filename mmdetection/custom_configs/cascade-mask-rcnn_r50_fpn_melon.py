311874291380b1969e8fc6e7d5e9fb845e102386
wj.jeong <wj.jeong@itconv.co.kr>
2025-04-07 11:09:07 +0900

    melon 학습 설정 수정

--- mmdetection/custom_configs/cascade-mask-rcnn_r50_fpn_melon.py
+++ mmdetection/custom_configs/cascade-mask-rcnn_r50_fpn_melon.py
@@ -1,6 +1,6 @@
 _base_ = [
     "_base_/models/cascade-mask-rcnn_r50_fpn.py",
-    "_base_/datasets/onion_instance.py",
+    "_base_/datasets/melon_instance.py",
     "_base_/schedules/schedule_1x.py",
     "_base_/default_runtime.py"
 ]
